eb27c5d38f6b2994934e6f4040ca1de0199df92a
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-09-10 19:48:48 -0400

    Added onto selectionsort function

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 
 // Function to perform Selection Sort
-void selectionSort(int arr[], int n) {}
+void selectionSort(int arr[], int n) {
+
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; 
+}
